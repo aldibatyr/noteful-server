c33b36c97f38c5ba737aec207ce550cb653b4020
aldibatyr <aldibatyrbekov@gmail.com>
2019-09-05 07:41:04 -0700

    changed db to database

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'developement',
-  DB_URL: process.env.DB_URL || 'postgres://noty@localhost/noteful'
+  DB_URL: process.env.DATABASE_URL || 'postgres://noty@localhost/noteful'
 };
